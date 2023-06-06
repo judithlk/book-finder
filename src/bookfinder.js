5522235d9308045afe1b1cb47571b27f14905427
Judith Yusuf <judithyusuf00@gmail.com>
2023-06-06 23:22:05 +0100

    This is my second commit. CSS improved, bug fixed.

--- src/bookfinder.js
+++ src/bookfinder.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from 'axios';
-import { Card } from "react-bootstrap";
 import './bookfinder.css';
 import { Search } from "react-bootstrap-icons";
+// import xtype from 'xtypejs';
 //import { responsivePropType } from "react-bootstrap/esm/createUtilityClasses";
 
 function BookFinder() {
@@ -12,24 +12,27 @@ function BookFinder() {
     const changeHandler = (e) => {
         setInputTerm(e.target.value);
     }
-    const authorsConcat = x => {
-        if(x.length <= 2) {
-            authorsNames = x.join(" & ");
-        }else if(x.length > 2) {
-            let lastName = ' & ' + x.slice(-1);
-            x.pop();
-            authorsNames = x.join(", ");
-            authorsNames += lastName;
+    const authorsConcat = (x) => {
+        if(x !== undefined){
+            if(x.length <= 2) {
+                authorsNames = x.join(" & ");
+            }else if(x.length > 2) {
+                let lastName = ' & ' + x.slice(-1);
+                x.pop();
+                authorsNames = x.join(", ");
+                authorsNames += lastName;
+            }
+        }else{
+            authorsNames = "Author unavailable";
         }
         return authorsNames;
     } 
 
     const submitHandler = (e) => {
         e.preventDefault();
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${inputTerm}&key=${process.env.REACT_APP_GB_API_KEY}&maxResults=40`)
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${inputTerm}&key=${process.env.REACT_APP_GB_API_KEY}&maxResults=20`)
         
         .then(response => {
-            console.log(response.data.items);
             setResult(response.data.items);
         })
         .catch(err => {
@@ -49,15 +52,15 @@ function BookFinder() {
             <div className="container">
                 <div className="row result-box">
                     {result.map(inputTerm => (
-                        <div key={inputTerm.id} className="col-sm-3 card-box">
-                            <Card style={{ 'marginTop': '10px'}}>
-                                <Card.Img variant="top" src={inputTerm.volumeInfo.imageLinks !== undefined ? inputTerm.volumeInfo.imageLinks.thumbnail : ''} alt={inputTerm.volumeInfo.title + " cover"} />
-                                <Card.Body>
-                                    <h5 className="card-title title-display">{inputTerm.volumeInfo.title}</h5>
-                                    {/* <small>{authorsConcat(inputTerm.volumeInfo.authors)}</small> */}
-                                    <a href={inputTerm.volumeInfo.canonicalVolumeLink} target="_blank" rel="noreferrer" className="btn primary-btn">Know More</a>
-                                </Card.Body>
-                            </Card>
+                        <div key={inputTerm.id} className="col-sm-4 col-lg-3 card-box">
+                            <div className="image-display">
+                                <img src={inputTerm.volumeInfo.imageLinks !== undefined ? inputTerm.volumeInfo.imageLinks.thumbnail : ''} alt={inputTerm.volumeInfo.title + " cover"}/>
+                            </div>
+                            <div className="info-display">
+                                <h5 className="title-display">{inputTerm.volumeInfo.title}</h5>
+                                <h6 className="author-display">{authorsConcat(inputTerm.volumeInfo.authors)}</h6>
+                            </div>
+                                <a href={inputTerm.volumeInfo.canonicalVolumeLink} target="_blank" rel="noreferrer" className="link-button"><button>Know More</button></a>
                         </div>
                     ))}
                 </div>
